3fd41849d6fa168c495bd3330b15d9d51ed9f027
Wiloke <contact.wilcity@gmail.com>
2020-12-29 11:45:32 +0700

    test

--- .circleci/install-wp-cli.sh
+++ .circleci/install-wp-cli.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root --path=wp
-./wp-cli.phar core config --allow-root --dbname=wordpress --dbuser=wordpress --dbhost=127.0.0.1 --dbpass=wordpress --path=wp
-./wp-cli.phar core install --allow-root --admin_name=admin --admin_password=admin --admin_email=admin@example.com --url=${WP_HOST} --title=WordPress --path=wp
+./wp-cli.phar core download --path=wp
+./wp-cli.phar core config create --dbname=wordpress --dbuser=wordpress --dbhost=127.0.0.1 --dbpass=wordpress --path=wp
+./wp-cli.phar core install --admin_name=admin --admin_password=admin --admin_email=admin@example.com --url=${WP_HOST} --title=WordPress
