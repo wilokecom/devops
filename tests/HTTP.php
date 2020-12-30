a3434690ccd44cbc99af8ef472ad110c966cdf5c
Wiloke <contact.wilcity@gmail.com>
2020-12-30 22:03:10 +0700

    oke

--- tests/HTTP.php
+++ tests/HTTP.php
@@ -1,34 +1,35 @@
 <?php
 
 
-	namespace Tests;
+namespace Tests;
 
 
-	trait HTTP
-	{
-		public function post(array $aArgs)
-		{
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, admin_url('admin-ajax.php'));
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($aArgs));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-			$output = curl_exec($ch);
+trait HTTP
+{
+    public function post(array $aArgs)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, admin_url('admin-ajax.php'));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($aArgs));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        $output = curl_exec($ch);
 
-			if (curl_errno($ch)) {
-				$errMsg = curl_error($ch);
-			}
-			curl_close($ch);
+        if (curl_errno($ch)) {
+            $errMsg = curl_error($ch);
+        }
+        curl_close($ch);
 
-			var_export($output);die;
-			if (isset($errMsg)) {
-				return [
-					'success' => false,
-					'msg'     => $errMsg
-				];
-			}
+        var_export($output);
+        die;
+        if (isset($errMsg)) {
+            return [
+                'success' => false,
+                'msg' => $errMsg
+            ];
+        }
 
-			return json_decode($output, true);
-		}
-	}
+        return json_decode($output, true);
+    }
+}
